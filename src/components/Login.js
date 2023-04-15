75c5c6edad46620a4ef86e0e67f3b66269d96a77
jzf21 <jozefneelamkavil2003@gmail.com>
2023-04-15 22:24:15 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,81 @@
 import React from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("allenshibu@outlook.in");
+    const [password, setPassword] = useState("hello123");
+
+    const signup = async (e) => {
+      e.preventDefault();
+
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    };
+
+    const login = (e) => {
+      e.preventDefault();
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    };
+
+    const logout = () => {
+      signOut(auth)
+        .then(() => {
+          console.log("Signed out successfully");
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+    };
+
   return (
-    <div className="h-[100vh] ">
-      <div className="flex flex-col items-center justify-center h-[100vh]">
+    <div className="h-[100vh] flex  flex-row justify-evenly ">
+      <div className="flex flex-col items-center  justify-center h-[100vh]">
         <h1 className="text-4xl font-bold">Login</h1>
-        <div className="flex flex-col items-center justify-center mt-10">
+        <div className="flex flex-col mt-10">
+          <label className="text-xl ">Email</label>
           <input
+        
             type="text"
-            placeholder="Username"
-            className="w-80 h-10 px-5 mb-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            placeholder="johndoe@gmail.com"
+            className="w-80 h-10 mb-5 "
+                onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
+          <label className="text-xl ">Password</label>
           <input
             type="password"
-            placeholder="Password"
-            className="w-80 h-10 px-5 mb-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+            placeholder="********"
+            className="w-80 h-10  mb-5 "
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
-          <button className="w-80 h-10 px-5 mb-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent">
+          <button className="w-[120px] bg-blue-500 text-white py-2 px-4">
             Login
           </button>
         </div>
       </div>
+      <img
+        className=""
+        src="https://stories.freepiklabs.com/api/vectors/health-professional-team/amico/render?color=407BFFFF&background=complete&hide="
+      />
     </div>
   );
 };
