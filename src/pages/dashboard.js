ef136317009973024eb05a2edb89cc7072edc8b0
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-04-15 16:22:47 +0530

    Add dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,54 @@
-import Navbar from "@/components/Navbar";
+import { useState } from "react"
+
+import { FaStop, FaPlay, FaPause } from "react-icons/fa"
+
+import Navbar from "@/components/Navbar"
 
 export default function Dashboard() {
-  return <div className="h-screen">
-    <Navbar />
+  const [symptoms, setSymptoms] = useState("")
+  const [diagnosis, setDiagnosis] = useState("")
 
-  </div>
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar />
+      <div className="h-full bg-green-200 flex flex-col justify-evenly items-center">
+        <div className="w-full flex flex-row justify-evenly items-center">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <p className="text-3xl">Symptoms</p>
+            <textarea
+              className="h-48 w-96"
+              onChange={(e) => {
+                setSymptoms(e.target.value)
+              }}
+              placeholder="Please start recording to generate symptoms"
+            ></textarea>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4">
+            <p className="text-3xl">Diagnosis</p>
+            <textarea
+              className="h-48 w-96"
+              onChange={(e) => {
+                setDiagnosis(e.target.value)
+              }}
+              placeholder="Please start recording to generate diagnosis"
+            ></textarea>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <p className="text-2xl">Recording Details</p>
+          <div className="flex flex-row gap-4 text-2xl">
+            <button>
+              <FaPlay />
+            </button>
+            <button>
+              <FaPause />
+            </button>
+            <button>
+              <FaStop />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
