7327ca0bed5652ebe7e3f39b6f6d4a84da899b9a
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-04-15 16:29:28 +0530

    Add patient details

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -11,6 +11,17 @@ export default function Dashboard() {
   return (
     <div className="h-screen flex flex-col">
       <Navbar />
+      <div className="py-10 px-4 flex flex-col gap-4">
+        <p className="text-2xl">Patient Details</p>
+        <div className="flex flex-col justify-center items-start gap-4 text-3xl">
+          <p>
+            Name: <span className="font-bold">Aldrin Jenson</span>
+          </p>
+          <p>
+            Age: <span className="font-bold">21</span>
+          </p>
+        </div>
+      </div>
       <div className="h-full bg-green-200 flex flex-col justify-evenly items-center">
         <div className="w-full flex flex-row justify-evenly items-center">
           <div className="flex flex-col justify-center items-center gap-4">
