458eb2db12e17f14931801ab721dec8f5acb235d
Aldrin Jenson <aldrinjenson@gmail.com>
2023-04-16 11:40:17 +0530

    add cases page

--- src/pages/patients/[id].js
+++ src/pages/patients/[id].js
@@ -1,17 +1,6 @@
-import { generatePdf } from "@/utils/generateReport";
 import { useEffect } from "react";
 
 const PatientDetails = () => {
-  useEffect(() => {
-    generatePdf({
-      name: "Johhny Doe",
-      bloodGroup: "B -ve",
-      contactNumber: "99876335",
-      address: "sdfdsfdsf,ds,fds,fds,f,dsf",
-      dateOfBirth: "16/3/2332",
-      imgUrl: "sdf",
-    });
-  });
   return <div>PatientDetails</div>;
 };
 
